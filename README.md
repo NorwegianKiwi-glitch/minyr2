92969893b142a9fda5ad848de87ce930f9215c66
NorwegianKiwi_glitch <simonhelgen1@gmail.com>
2023-03-31 17:01:33 +0200

    convert

--- README.md
+++ README.md
@@ -3,4 +3,16 @@
 Porblemer med **imports github.com/NorwegianKiwi-glitch/Funtemps/conv: import cycle not allowed**
     Fikset det med å lage ett nytt reposetory som med navn "minyr2" istedet for "minyr" og bruker "github.com/NorwegianKiwi-glitch/minyr2" i go.mod isetedet for "github.com/NorwegianKiwi-glitch/funtemps/conv"
 
+Problemer med importering av **github.com/NorwegianKiwi-glitch**
+    PS C:\Users\simon\reps\IS-105\go\minyr2> go get github.com/NorwegianKiwi-glitch/Funtemps/conv
+    go: github.com/NorwegianKiwi-glitch/Funtemps@v0.0.0-20230330101830-93228ad389d7: parsing go.mod:
+        module declares its path as: github.com/NorwegianKiwi-glitch/funtemps
+                but was required as: github.com/NorwegianKiwi-glitch/Funtemps
+    
+    Ting jeg har prøvd
+        - Gjøre reposetoriet åpent
+        - GOPROXY 
+        - lage nytt reposetory
+        - Feilsøking og hodeskraing i mange timer
+
 Se mer på test "en test som sjekker at gjennomsnittempraturen er 8.56"
--- main.go
+++ main.go
@@ -2,140 +2,45 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
-	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"strconv"
 	"strings"
+
+	"github.com/NorwegianKiwi-glitch/minyr2/yr"
 )
 
-func celsiusToFahrenheit(celsius float64) float64 {
-	return celsius*1.8 + 32
-}
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("> ")
-		if !scanner.Scan() {
-			// Scanner encountered an error or EOF
-			break
-		}
-		command := scanner.Text()
-
-		switch strings.ToLower(command) {
-		case "minyr":
-			// Kill all foreground processes
-			cmd := exec.Command("pkill", "-f", "")
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err := cmd.Run(); err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to kill foreground processes: %v\n", err)
-			}
-		case "q", "quit":
-			// Exit the loop and terminate the program
-			fmt.Println("Exiting...")
-			return
-		default:
-			// Unknown command, print an error message
-			fmt.Println("Unknown command:", command)
-		}
+	// Wait for the user to enter "minyr"
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter 'minyr' to start temperature conversion: ")
+	text, _ := reader.ReadString('\n')
+	if strings.ToLower(strings.TrimSpace(text)) != "minyr" {
+		fmt.Println("Invalid input.")
+		return
 	}
-	// Define command-line flags
-	convertFlag := flag.Bool("convert", false, "convert temperature data from Celsius to Fahrenheit")
-	flag.Parse()
 
-	if *convertFlag {
-		// Check if output file already exists
-		if _, err := os.Stat("kjevik-temp-celsius-20220318-20230318.csv"); !os.IsNotExist(err) {
-			// Output file already exists, prompt user to regenerate
-			var regenerate string
-			fmt.Print("Output file already exists. Regenerate? (j/n): ")
-			fmt.Scanln(&regenerate)
-			if regenerate != "j" && regenerate != "J" {
-				fmt.Println("Exiting without generating new file.")
-				return
-			}
-		}
+	// Present the user with options
+	fmt.Println("Options:")
+	fmt.Println("  - 'convert' to convert temperature data from Celsius to Fahrenheit")
+	fmt.Println("  - 'average' to get the average temperature for the entire period")
+	fmt.Print("Enter option: ")
+	option, _ := reader.ReadString('\n')
+	option = strings.ToLower(strings.TrimSpace(option))
 
-		// Open the input CSV file
-		inputFile, err := os.Open("yr/kjevik-temp-celsius-20220318-20230318.csv")
+	if option == "convert" {
+		err := yr.Convert()
 		if err != nil {
-			fmt.Println("Error opening input file:", err)
-			return
-		}
-		defer inputFile.Close()
-
-		// Create a new scanner to read the input CSV file
-		inputScanner := bufio.NewScanner(inputFile)
-
-		// Create a new CSV writer to write the output CSV file
-		outputFile, err := os.Create("yr/kjevik-temp-fahr-20220318-20230318.csv")
-		if err != nil {
-			fmt.Println("Error creating output file:", err)
-			return
-		}
-		defer outputFile.Close()
-
-		outputWriter := csv.NewWriter(outputFile)
-		defer outputWriter.Flush()
-
-		// Skriv ut den første linjen fra inputfilen til outputfilen
-		if inputScanner.Scan() {
-			firstLine := inputScanner.Text()
-			if err = outputWriter.Write(strings.Split(firstLine, ";")); err != nil {
-				fmt.Println("Error writing first line:", err)
-				return
-			}
-		}
-
-		// Loop through each line of the input CSV file
-		for inputScanner.Scan() {
-			// Split the line into fields
-			fields := strings.Split(inputScanner.Text(), ";")
-
-			// Check that the fields slice has at least 4 elements
-			if len(fields) < 4 {
-				fmt.Println("Error: Invalid input format.")
-				continue
-			}
-
-			// Extract the last digit from the fourth column
-			temperature, err := strconv.ParseFloat(fields[3], 64)
-			if err != nil {
-				fmt.Println("Error parsing temperature:", err)
-				continue
-			}
-			lastDigit := temperature - float64(int(temperature/10))*10
-
-			// Convert Celsius to Fahrenheit
-			fahrenheit := celsiusToFahrenheit(lastDigit)
-
-			// Replace the temperature in the fourth column with the converted value
-			temperatureString := strconv.FormatFloat(fahrenheit, 'f', 2, 64)
-			temperatureParts := strings.Split(temperatureString, ".")
-			fields[3] = temperatureParts[0] + "." + string(temperatureParts[1][0])
-
-			// Write the updated line to the output CSV file
-			err = outputWriter.Write(fields)
-			if err != nil {
-				fmt.Println("Error writing line to output file:", err)
-				continue
-			}
-		}
-
-		dataText := "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET); endringen er gjort av Simon Helgen"
-		err = outputWriter.Write([]string{dataText})
-		if err != nil {
-			fmt.Println("Error writing data text to output file:", err)
+			fmt.Println("Error during temperature conversion:", err)
 			return
 		}
 		fmt.Println("Temperature conversion complete.")
 		return
 	}
+
+	if option == "average" {
+		fmt.Println("Average")
+	}
+
 	// Wait for user input
 	fmt.Println("Press enter to exit.")
 	fmt.Scanln()
